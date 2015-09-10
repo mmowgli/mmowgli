200867aba7a5471dab984346f82d6f552267fb38
blueoak <blueoak@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2015-09-10 16:30:43 +0000

    More marker annotations

---
+++ application/trunk/src/edu/nps/moves/mmowgli/markers/HibernateCardSave.java
@@ -0,0 +1,31 @@
+/*
+  Copyright (C) 2010-2014 Modeling Virtual Environments and Simulation
+  (MOVES) Institute at the Naval Postgraduate School (NPS)
+  http://www.MovesInstitute.org and http://www.nps.edu
+ 
+  This file is part of Mmowgli.
+  
+  Mmowgli is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Mmowgli is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Mmowgli in the form of a file named COPYING.  If not,
+  see <http://www.gnu.org/licenses/>
+*/
+
+package edu.nps.moves.mmowgli.markers;
+
+/**
+ * This is a method marker for methods that use Hibernate.
+ * 
+ *@author jmbailey@nps.edu
+ */
+
+public @interface HibernateCardSave {}
