919a2be70f7d7a2221df2b0b3a1ab494625932cd
blueoak <blueoak@03f8cbdb-1dfa-4f6b-8dc2-9792cfd65271>
2015-10-27 21:14:17 +0000

    support resource logging

--- application/trunk/WebContent/VAADIN/instrument/mmowgli1.js
+++ application/trunk/WebContent/VAADIN/instrument/mmowgli1.js
@@ -1,14 +1,31 @@
 function _mmowgliFinishedLoading() {
+	console.log("_mmowgliFinishedLoading() called");
 	// Set css for dbug divs
 	var node = document.createElement('style');
 	node.innerHTML = '#triggerDbugDiv {position:fixed;bottom:10px;left:10px;width:10px;height:10px;z-index:10000;} \
 	 #triggerDbugDiv:hover {cursor:pointer;} \
 	 #displayDiv {position:fixed; top:0px; left:0px; font-family:monospace; font-size:10px; background:white; border: 2px solid black; z-index:10000; word-break: break-all;}';
 	document.body.appendChild(node);
+	
+	shipStats();
 }
 
+var userInfoData = null;
+UserInfo.getInfo(
+	function(data) {
+	    userInfoData = data;
+    },
+    function(err){
+        console.log("UserInfo error: "+err);
+    }
+);
+
+
 // executed inline
-if (window.performance.getEntriesByType && document.addEventListener)
+//if (window.performance.getEntriesByType && document.addEventListener)
+if(!(typeof(window.performance) == "undefinded") &&
+   !(typeof(window.performance.getEntriesByType) == "undefined") &&
+   !(typeof(document.addEventListener) == 'undefined')) 
 	document.addEventListener('DOMContentLoaded', _mmowgliFinishedLoading);
 
 var displayDiv;
@@ -47,4 +64,60 @@ function triggerDbugDivMouseDown() {
 			displayDiv.innerHTML = dataString;				
 		}
 	}
+}
+
+// Javascript functions called from Java
+var doShipStats = true;  // starts off true
+
+function shipStatsOn() {
+	doShipStats = true;
+}
+function shipStatsOff() {
+	doShipStats = false;
+}
+
+function shipStats() {
+	if(!doShipStats)
+		return;
+	
+	if(typeof(edu_nps_moves_mmowgli_utility_browserperformancelogger_loadstatscallback) == "undefined") {
+		setTimeout("shipStats()",250);
+	}
+	else {
+		var stats = {};
+		if(userInfoData) {
+			stats.clientip = userInfoData.ip_address;
+			stats.location = userInfoData.city.name+","+userInfoData.continent.name+","+userInfoData.country.name;
+		}
+		else {
+			stats.clientip = "unknown";
+			stats.location = "unknown";
+		}
+		stats.browser = navigator.userAgent;
+		stats.postdatetime = myformat(new Date());
+
+		var arr = [];
+		stats.resources = arr;
+		
+		var perfs = window.performance.getEntriesByType("resource"); 
+		var numberOfResources = perfs.length;
+		for(var idx = 0; idx < numberOfResources; idx++) {
+			entry = perfs[idx];
+			var line = {
+				"url":entry.name,
+				"duration":entry.duration
+				}
+            stats.resources.push(line);
+		}
+		
+		edu_nps_moves_mmowgli_utility_browserperformancelogger_loadstatscallback(stats);
+		
+		if(window.performance.clearResourceTimings)
+			window.performance.clearResourceTimings();
+	}
+}
+
+function myformat(date) {
+	return date.getMonth()+"/"+date.getDate()+   "/"+date.getFullYear()+" "+
+	       date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+ "."+date.getMilliseconds();
 }
